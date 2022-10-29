d9004a533eb15c7c5b191fd2e3364693ac4096cf
mack <mack_ding@outlook.com>
2022-10-29 18:35:22 +0800

    increase

---
+++ ex31.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# @Time : 29/10/22 6:31 PM
+# @Author: Mack.Ding
+# @File : ex31.py
+
+print("""You enter a dark room with two doors. Do you go through door #1 or door #2?""")
+
+door = input("> ")
+
+if door == "1":
+    pass
