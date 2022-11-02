31ca5aa54d0043c4c626cd37e12ba3f49cc084d2
“ran.ding” <ran.ding@extremevision.com.cn>
2022-11-02 14:11:25 +0800

    update

---
+++ ex32.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.8
+# -*- coding: utf-8 -*-
+# @Author: Mack
+# @Time: 2022/11/2 13:18 
+# @File: ex32.py
+# @Software: PyCharm
+
+
+hairs = ['brown', 'blond', 'red']
+eyes = ['brown', 'blue', 'green']
+weights = [1, 2, 3, 4]
+
+
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+
---
+++ ex33.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.8
+# -*- coding: utf-8 -*-
+# @Author: Mack
+# @Time: 2022/11/2 13:18 
+# @File: ex33.py
+# @Software: PyCharm
